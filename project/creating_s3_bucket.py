6c8ceb0c3be1b0786eec11cda242f39254397eab
praneethmetuku <metuku0507@gmail.com>
2018-10-24 16:15:46 -0500

    simple code for s3_bucket in boto3

---
+++ project/creating_s3_bucket.py
@@ -0,0 +1,7 @@
+import boto3
+session=boto3.Session(profile_name='pythonautomation')
+ s3=session.resource('s3')
+ new_bucket=s3.create_bucket(Bucket='awspythonboto3-praneeth',CreateBucketConfiguration={'LocationConstraint':'us-east-2'})
+ new_bucket
+for i in s3.buckets.all():
+    print(i)
---
+++ project/simples3_boto3.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import boto3
+session=boto3.Session(profile_name='pythonautomation') 
+s3=session.resource('s3')
+new_bucket=s3.create_bucket(Bucket='awspythonboto3-praneeth',CreateBucketConfiguration={'LocationConstraint':'us-east-1'})
+new_bucket=s3.create_bucket(Bucket='awspythonboto3-praneeth',CreateBucketConfiguration={'LocationConstraint':'us-east-2'})
+new_bucket
+for i in s3.buckets.all():
+    print(i)
+get_ipython().run_line_magic('history', '')
